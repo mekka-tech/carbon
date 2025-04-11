8060a4180988e3948963a63dbc9e3e190bfcec72
darienmh <darienmh@gmail.com>
2025-04-11 15:20:34 -0500

    test balance

--- examples/alerts/src/pumpfun/pumpfun_new_tokens.rs
+++ examples/alerts/src/pumpfun/pumpfun_new_tokens.rs
@@ -91,16 +91,13 @@ impl Processor for PumpfunNewTokensInstructionProcessor {
     ) -> CarbonResult<()> {
         let signature = metadata.transaction_metadata.signature;
         let accounts = instruction.accounts;
-        
-        let pre_balance = metadata.transaction_metadata.meta.pre_balances[0];
-        let post_balance = metadata.transaction_metadata.meta.post_balances[0];
-        
-        // println!("Pre Balance: {}", pre_balance);
-        // println!("Post Balance: {}", post_balance);
 
         match instruction.data {
             PumpfunInstruction::Create(create) => match Create::arrange_accounts(&accounts) {
                 Some(accounts) => {
+                    let pre_balance = metadata.transaction_metadata.meta.pre_balances[0];
+                    let post_balance = metadata.transaction_metadata.meta.post_balances[0];
+
                     if post_balance > pre_balance {
                         return Ok(());
                     }
