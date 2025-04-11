e8629d00827de71a9a7d143fcd31da2fb2552fde
darienmh <darienmh@gmail.com>
2025-04-11 01:50:53 -0500

    try retry

--- swap/src/pump/utils.ts
+++ swap/src/pump/utils.ts
@@ -60,6 +60,7 @@ export const blockchainErrorMap: Record<string, string> = {
   "0x1765": "Token migrated to Raydium.",
   "0x1": "Another error occurred",
   "6": "IncorrectProgramId",
+  "6002": "Slippage, need more SOL",
 };
 
 export function genericBlockchainError(code: string): string {
